75c4da55ad6434ef959df8c823ce6562bcf1f392
Andres Elizondo <acesxx@gmail.com>
2017-07-26 22:10:24 +0200

    Removed PrintFunctions

--- app.js
+++ app.js
@@ -15,89 +15,15 @@ const ERROR_INTERVAL = 2121;
 const URLMAX_TIMEOUT = 2112;
 const HOST = 'https://www.ticketswap.nl';
 //const EVENT_URL = '/event/fiesta-macumba/0f4bf5d9-a5b5-46b2-8f15-0942ef96d1e1';
-//const EVENT_URL = '/event/milkshake-festival-2017/sunday/68fc1cc6-6b98-4c4c-b828-2c7fa712c579/128371';
-const EVENT_URL = '/event/a-campingflight-to-lowlands-paradise/regular/fcc6c783-6b32-4abd-8fe6-e9d0369c14df/20635';
+const EVENT_URL = '/event/milkshake-festival-2017/sunday/68fc1cc6-6b98-4c4c-b828-2c7fa712c579/128371';
+//const EVENT_URL = '/event/a-campingflight-to-lowlands-paradise/regular/fcc6c783-6b32-4abd-8fe6-e9d0369c14df/20635';
 
 // APP VARIABLES
 let sleepTime;
 let soldListings;
 
-// HTTP REQUESTS
-let cookieJar = request.jar();
-let buildRequest = function (uri, method) {
-    return request({
-        uri: uri,
-        method: method,
-        jar: cookieJar,
-        headers: {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
-            'Cache-Control': 'max-age=0'
-        },
-        timeout: URLMAX_TIMEOUT
-    });
-};
-let fetchResult = function (link) {
-    return co(function *() {
-        let result = yield buildRequest(link, 'GET');
-        return cheerio.load(result.body);
-    });
-};
-
-// EXECUTION FUNCTIONS
-let sleep = function (seconds) {
-    return new Promise(resolve => setTimeout(resolve, seconds * 1000)).then(() => { app(); });
-};
-let print = function (toPrint) {
-    console.log(`${new Date(Date.now()).toLocaleTimeString()}> ${toPrint}`);
-};
-
-// PRINTING FUNCTIONS
-let printAction = {
-    banner: function () {
-        print(`\n
-    ************************************************************************
-        Welcome to TicketSwap crawler!
-        Searching for tickets for: ${EVENT_URL.split('/')[2].replace(/-/g, ' ').toUpperCase()}
-    ************************************************************************\n`);
-    },
-    foundListings: function (length) {
-        print(`\n********************* Found ${length} available listing(s)! ********************`);
-    },
-    noTickets: function (updatedSoldListings) {
-        let newListings = '';
-        if (soldListings && updatedSoldListings != soldListings)
-            newListings = `${updatedSoldListings - soldListings} Ticket(s) recently sold.`;
-        print(`No new tickets found... ${updatedSoldListings} Sold. ${newListings}`);
-    },
-};
-
-// BOT FUNCTIONS
-let botAction = {
-    availableTicket: function (url, amount) {
-        exec(`open ${url}`);
-        sleepTime = FOUND_INTERVAL;
-        print(`${amount} TICKET(S) AVAILABLE!: \n${url}`);
-        return false; // STOPS 'EACH 'LOOP
-    },
-    reservedTicket: function (url) {
-        print(`TICKET RESERVED: \n${url}`);
-    },
-    noTickets: function (updatedSoldListings) {
-        printAction.noTickets(updatedSoldListings);
-        soldListings = updatedSoldListings;
-    },
-    robotCheck: function (url) {
-        sleepTime = ROBOT_INTERVAL;
-        print(`Need to check captcha: \n${url}`);
-    },
-    invalidURL: function (url) {
-        sleepTime = ERROR_INTERVAL;
-        print(`Invalid URL: \n${url}`);
-    },
-};
-
 // MAIN APP
-let app = function () {
+let ticketCrawler = function () {
     return co(function* () {
         sleepTime = CHECK_INTERVAL;
         let result = yield buildRequest(HOST + EVENT_URL, 'GET');
@@ -105,7 +31,7 @@ let app = function () {
 
         let availableListings = $('.listings-item:not(.listings-item--not-for-sale) a');
         if (availableListings.length > 0) {
-            printAction.foundListings(availableListings.length);
+            print(`\n    ********************* Found ${availableListings.length} available listing(s)! *******************`);
 
             let linksFn = {};
             availableListings.each((index, listing) => {
@@ -140,6 +66,66 @@ let app = function () {
     });
 };
 
+// BOT FUNCTIONS
+let botAction = {
+    availableTicket: function (url, amount) {
+        exec(`open ${url}`);
+        sleepTime = FOUND_INTERVAL;
+        print(`${amount} TICKET(S) AVAILABLE!: \n${url}`);
+        return false; // STOPS 'EACH 'LOOP
+    },
+    reservedTicket: function (url) {
+        print(`TICKET RESERVED: \n${url}`);
+    },
+    noTickets: function (updatedSoldListings) {
+        if (soldListings && updatedSoldListings != soldListings)
+            var newListings = `${updatedSoldListings - soldListings} Ticket(s) recently sold.`;
+        print(`No new tickets found... ${updatedSoldListings} Sold. ${newListings || ''}`);
+        soldListings = updatedSoldListings;
+    },
+    robotCheck: function (url) {
+        sleepTime = ROBOT_INTERVAL;
+        print(`Need to check captcha: \n${url}`);
+    },
+    invalidURL: function (url) {
+        sleepTime = ERROR_INTERVAL;
+        print(`Invalid URL: \n${url}`);
+    },
+};
+
+// REQUEST FUNCTIONS
+let cookieJar = request.jar();
+let buildRequest = function (uri, method) {
+    return request({
+        uri: uri,
+        method: method,
+        jar: cookieJar,
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
+            'Cache-Control': 'max-age=0'
+        },
+        timeout: URLMAX_TIMEOUT
+    });
+};
+let fetchResult = function (link) {
+    return co(function *() {
+        let result = yield buildRequest(link, 'GET');
+        return cheerio.load(result.body);
+    });
+};
+
+// EXECUTION FUNCTIONS
+let sleep = function (seconds) {
+    return new Promise(resolve => setTimeout(resolve, seconds * 1000)).then(() => { ticketCrawler(); });
+};
+let print = function (toPrint) {
+    console.log(`${new Date(Date.now()).toLocaleTimeString()}> ${toPrint}`);
+};
+
 // INITIALIZE
-printAction.banner();
-app();
+print(`\n
+    ************************************************************************
+    Welcome to TicketSwap crawler!
+    Searching for tickets for: ${EVENT_URL.split('/')[2].replace(/-/g, ' ').toUpperCase()}
+    ************************************************************************\n`);
+ticketCrawler();
